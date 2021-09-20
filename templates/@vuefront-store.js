4a3e29e5a1daa66560be10a41fedae8165d54a28
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-09-20 15:37:01 +0300

    fix: fix bug with lodash

--- templates/@vuefront-store.js
+++ templates/@vuefront-store.js
@@ -1,11 +1,11 @@
-import _ from 'lodash'
+import {set} from 'lodash'
 import {createStore} from 'vuex'<% for (var key in options.themeOptions.store) { %><% if (typeof options.themeOptions.store[key].module !== 'undefined') {%><% if (options.themeOptions.store[key].module.type === 'full') { %>
 import * as <%= key %> from '<%= options.themeOptions.store[key].module.path %>'<% } else { %>
 import {<%= options.themeOptions.store[key].module.component %> as <%= key %>} from <%= options.themeOptions.store[key].module.path %><% } %><% } %><% } %>
 export default (ctx, inject) => {
   let modules = {}
   <% for (var key in options.themeOptions.store) { %><% if (typeof options.themeOptions.store[key].module !== 'undefined') {%>
-  modules = _.set(modules, <%= JSON.stringify(options.themeOptions.store[key].path) %>, { namespaced: true, ...<%= key %>})<% } else { %>
-    modules = _.set(modules, <%= JSON.stringify(options.themeOptions.store[key].path) %>, { namespaced: true})<% } %><% } %>
+  modules = set(modules, <%= JSON.stringify(options.themeOptions.store[key].path) %>, { namespaced: true, ...<%= key %>})<% } else { %>
+    modules = set(modules, <%= JSON.stringify(options.themeOptions.store[key].path) %>, { namespaced: true})<% } %><% } %>
   return createStore({modules})
 }
