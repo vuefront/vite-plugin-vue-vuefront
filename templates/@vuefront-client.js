a46f17abb231ca80a8878a04652c9f1facd0594a
Aleksandr Krasnyi <aleksandr.krasnyi@dreamvention.com>
2021-11-28 19:27:03 +0200

    fix: fix bug

--- templates/@vuefront-client.js
+++ templates/@vuefront-client.js
@@ -3,6 +3,12 @@ import {seoResolver} from '@vuefront-seo-resolver'
 export default async (app) => {
   app.$router.beforeEach((to, from, next) => {
     seoResolver.call(app, app, to, from, next)
+
+    if (to.path !== from.path) {
+      if (app.$store.getters["vuefront/sidebarCart"]) {
+        app.$store.commit("vuefront/toggleSidebarCart");
+      }
+    }
   })
   await seoResolver.call(app, app, app.$router.currentRoute, app.$router.currentRoute, (path) => {
     if (path) {
